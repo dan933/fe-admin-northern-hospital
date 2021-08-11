55d8d1a14f27ccaa315c5ff53f578863de9a360e
dan933 <69749413+dan933@users.noreply.github.com>
2021-08-11 12:19:10 +1000

    anxiety graph legend improvements and legend scroll implamented

--- nh-admin-application/src/app/services/chart.service.ts
+++ nh-admin-application/src/app/services/chart.service.ts
@@ -65,13 +65,29 @@ export class ChartService {
     
 
     // X and Y Axis Initialisation
+    let questions:any = {d1Data:[],d2Data:[],d3Data:[],d4Data:[],d5Data:[],d6Data:[], d7Data:[],d8Data:[],
+                        a1Data:[],a2Data:[],a3Data:[],a4Data:[],a5Data:[],a6Data:[],a7Data:[],a8Data:[]}
+    questions.d1Data.push("D1")
+    questions.d2Data.push("D2")
+    questions.d3Data.push("D3")
+    questions.d4Data.push("D4")
+    questions.d5Data.push("D5")
+    questions.d6Data.push("D6")
+    questions.d7Data.push("D7")
+    questions.d8Data.push("D8")
+    questions.a1Data.push("A1")
+    questions.a2Data.push("A2")
+    questions.a3Data.push("A3")
+    questions.a4Data.push("A4")
+    questions.a5Data.push("A5")
+    questions.a6Data.push("A6")
+    questions.a7Data.push("A7")
+    questions.a8Data.push("A8")
+
     let dateData:any = []
     dateData.push("type")
-
-    let d1Data:any = []
-    let d2Data:any = []
-    d1Data.push("D1")
-    d2Data.push("D2")
+    
+    
 
     //Date Column
     let questionareDate:string = 'questionare_date';
@@ -86,8 +102,22 @@ export class ChartService {
       date = input[row][questionareDate]
 
       //add Y Axis Data
-      d1Data.push(input[row]['d1']);
-      d2Data.push(input[row]['d2']);      
+      questions.d1Data.push(input[row]['d1']);
+      questions.d2Data.push(input[row]['d2']); 
+      questions.d3Data.push(input[row]['d3']);     
+      questions.d4Data.push(input[row]['d4']);     
+      questions.d5Data.push(input[row]['d5']);     
+      questions.d6Data.push(input[row]['d6']);     
+      questions.d7Data.push(input[row]['d7']);     
+      questions.d8Data.push(input[row]['d8']);     
+      questions.a1Data.push(input[row]['a1']);     
+      questions.a2Data.push(input[row]['a2']);     
+      questions.a3Data.push(input[row]['a3']);     
+      questions.a4Data.push(input[row]['a4']);     
+      questions.a5Data.push(input[row]['a5']);     
+      questions.a6Data.push(input[row]['a6']);     
+      questions.a7Data.push(input[row]['a7']);     
+      questions.a8Data.push(input[row]['a8']);     
       
       //add X Axis Data aka Dates
       dateData.push(date)
@@ -100,20 +130,37 @@ export class ChartService {
         type: 'none'
         }
       },
-      color: ["#003366","#ADD8E6"],
+      color: ['#2962ff', '#284987', '#001c76','#0045d8','#e75874','#0092b2','#195d7d','#01adb5','#2980b9',
+              '#bedb39','#7ecd55','#40a96a','#009777','#5fad8e','#246672','#7ae79a','#80c63b'],
       dataset: {
         source: [
           dateData,
-          d1Data,
-          d2Data
+          questions.d1Data,
+          questions.d2Data,
+          questions.d3Data,
+          questions.d4Data,
+          questions.d5Data,
+          questions.d6Data,
+          questions.d7Data,
+          questions.d8Data,
+          questions.a1Data,
+          questions.a2Data,
+          questions.a3Data,
+          questions.a4Data,
+          questions.a5Data,
+          questions.a6Data,
+          questions.a7Data,
+          questions.a8Data,
         ]
       },
-      legend: {  
+      legend: {
+        type:'scroll',
         itemWidth:30,
         itemHeight:30,
         icon:'square',
         selected:{
-          'D1':true,'D2':false}
+          'D1':true,'D2':false,'D3':false,'D4':false,'D5':false,'D6':false,'D7':false,'D8':false,
+          'A1':false,'A2':false,'A3':false,'A4':false,'A5':false,'A6':false,'A7':false,'A8':false}
       },
       xAxis: {
         type: "category",
@@ -124,10 +171,52 @@ export class ChartService {
       series: [{
         type: "scatter",
         seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },
+      {
+        type: "scatter",
+        seriesLayoutBy: "row",
+      }, {
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{      
+        type: "scatter",
+        seriesLayoutBy: "row",
+      },{
+        type:"scatter",
+        seriesLayoutBy:"row",      
       }, {
         type:"scatter",
-        seriesLayoutBy:"row",
-        seriesShow:false
+        seriesLayoutBy:"row",      
+      }, {       
+        type:"scatter",
+        seriesLayoutBy:"row",      
+      },{
+      type:"scatter",
+      seriesLayoutBy:"row",        
+      }, {
+      type:"scatter",
+      seriesLayoutBy:"row",        
       }],
         dataZoom: [
         {
@@ -152,6 +241,7 @@ export class ChartService {
         }
       ],
     }
+    console.log(questions.d1Data)
     
     return output;
   }
@@ -236,3 +326,6 @@ export class ChartService {
   }
 
 }
+
+
+//todo log colour change northern to #005cb9
